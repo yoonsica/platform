2c98de5e8c7df5cd3429d31be7d517b592a858a1
nigiss <547311673.qq.com>
2014-05-07 15:37:57 +0800

    格式化

--- src/main/java/com/ceit/vic/platform/controllers/MD5_Test.java
+++ src/main/java/com/ceit/vic/platform/controllers/MD5_Test.java
@@ -1,55 +1,54 @@
 package com.ceit.vic.platform.controllers;
+
 import java.security.*;
- 
+
 class MD5_Test {
-       public final static String MD5(String oldStr) {
-              char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                            'a', 'b', 'c', 'd', 'e', 'f' };
-                //System.out.println("原始字符串为："+oldStr);
-              try {
-                        // 参数oldStr表示要加密的字符串
-                        // 转换成字节流
-                     byte[] oldBytes = oldStr.getBytes();
- 
-for(byte b:oldBytes)
-{
-                     System.out.print(b+" ");
-}
-System.out.println();
- 
- 
-                        // 得到对象
-                        MessageDigest md = MessageDigest.getInstance("MD5");
- 
-                        // 初始化
-                     md.update(oldBytes);
- 
-                        // 运行加密算法
-                     byte[] newBytes = md.digest();
- 
-for(byte b:newBytes)
-{
-                     System.out.print(b+" ");
-}
-System.out.println();
- 
-                        // 构造长度为2倍的字符串
-                     char newStr[] = new char[32];
- 
-                        // 循环进行处理
-                     for (int i = 0; i < 16; i++) {
-                            byte tmp = newBytes[i];
-                            newStr[2*i] = hexDigits[tmp >>> 4 & 0xf];
-                            newStr[2*i+1] = hexDigits[tmp & 0xf];
-                     }
-                     System.out.println(newStr);
-                     return new String(newStr);
-              } catch (Exception e) {
-                     return null;
-              }
-       }
- 
-/*       public static void main(String[] args) {
-              System.out.println(MD5_Test.MD5("dyy*1234"));
-       }*/
+	public final static String MD5(String oldStr) {
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'a', 'b', 'c', 'd', 'e', 'f' };
+		// System.out.println("原始字符串为："+oldStr);
+		try {
+			// 参数oldStr表示要加密的字符串
+			// 转换成字节流
+			byte[] oldBytes = oldStr.getBytes();
+
+			for (byte b : oldBytes) {
+				System.out.print(b + " ");
+			}
+			System.out.println();
+
+			// 得到对象
+			MessageDigest md = MessageDigest.getInstance("MD5");
+
+			// 初始化
+			md.update(oldBytes);
+
+			// 运行加密算法
+			byte[] newBytes = md.digest();
+
+			for (byte b : newBytes) {
+				System.out.print(b + " ");
+			}
+			System.out.println();
+
+			// 构造长度为2倍的字符串
+			char newStr[] = new char[32];
+
+			// 循环进行处理
+			for (int i = 0; i < 16; i++) {
+				byte tmp = newBytes[i];
+				newStr[2 * i] = hexDigits[tmp >>> 4 & 0xf];
+				newStr[2 * i + 1] = hexDigits[tmp & 0xf];
+			}
+			System.out.println(newStr);
+			return new String(newStr);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	/*
+	 * public static void main(String[] args) {
+	 * System.out.println(MD5_Test.MD5("dyy*1234")); }
+	 */
 }
