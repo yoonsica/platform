ca1ee62103e1bb2fb1424ad637543a10e8d00b64
yoonsica <470614197@qq.com>
2014-12-04 21:24:02 +0800

    s

---
+++ src/test/java/com/ceit/vic/platform/service/Dep_PersonServiceTest.java
@@ -0,0 +1,14 @@
+package com.ceit.vic.platform.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Dep_PersonServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
